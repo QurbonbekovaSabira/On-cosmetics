6d0bd12c43f657a58f999acbad906b87062ebd27
devdamirbek@gmail.com <developerdamirbek@mail.ru>
2024-01-30 17:46:13 +0500

    again new change

--- src/layout/header/components/header-buttons/header-buttons.jsx
+++ src/layout/header/components/header-buttons/header-buttons.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { SearchIcon } from "../../../../assets/search-icon";
+import { SearchIcon } from "../../../../assets/iconsearch-icon";
 
 export const HeaderButtons = () => {
   return (
