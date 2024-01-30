859139a110481966a0097718c8ed8c9a537e3634
devdamirbek@gmail.com <developerdamirbek@mail.ru>
2024-01-30 22:15:42 +0500

    new change by damirbek

--- src/layout/footer/footer.jsx
+++ src/layout/footer/footer.jsx
@@ -1,5 +1,41 @@
 import React from "react";
+import style from './footer.module.scss'
+import { Logo } from "../../assets/icon/logo";
 
 export const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer>
+      <div className={style.footer_top}>
+        <div className="container">
+          <div className={style.footer_links}>
+            <div className={style.footer_logo}>
+              <Logo/>
+            </div>
+            <div className={style.footer_social__links}>
+              <a href="" className={style.social_link}>
+
+              </a>
+              <a href="" className={style.social_link}>
+
+              </a>
+              <a href="" className={style.social_link}>
+
+              </a>
+              <a href="" className={style.social_link}>
+
+              </a>
+              <a href="" className={style.social_link}>
+
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={style.footer_bottom}>
+        <div className="container">
+          <p className={style.footer_text}>© 2021 Beauty Store. All rights reserved.</p>
+        </div>
+      </div>
+    </footer>
+  );
 };
