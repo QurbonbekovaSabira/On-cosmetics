1731982db3757ea21386cc28a9750dd5adfb3dc9
QurbonbekovaSabira <sarakim2224@gmail.com>
2024-01-31 04:09:56 +0500

    icons

---
+++ src/assets/icon/left-Arrow-icon.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+// products page arrow left icon
+export const LeftArrowIcon = () => {
+  return (
+    <svg
+      width={16}
+      height={16}
+      viewBox="0 0 16 16"
+      fill="currentcolor"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M5.33314 1.06668L12.7998 8.00002L5.33314 14.9333"
+        stroke="black"
+        strokeLinecap="square"
+      />
+    </svg>
+  );
+};
--- src/assets/icon/logo.jsx
+++ src/assets/icon/logo.jsx
@@ -6,7 +6,7 @@ export const Logo = () => {
       width={80}
       height={80}
       viewBox="0 0 80 80"
-      fill="none"
+      fill="currentcolor"
       xmlns="http://www.w3.org/2000/svg"
     >
       <g clipPath="url(#clip0_2003_399)">
