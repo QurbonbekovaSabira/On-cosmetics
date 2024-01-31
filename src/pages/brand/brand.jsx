2d006f2332c259b801eab02b256b2a31958fc247
Abduhoshimov <muhammadyusufabduhoshimov@gmail.com>
2024-01-31 17:01:57 +0500

    changes

--- src/pages/brand/brand.jsx
+++ src/pages/brand/brand.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Hero } from '../../components/hero';
 import styles from "./brand.module.scss";
+import brandSection3Img from "../../assets/img/brand_section3_img.png";
+import brandSection4Img from "../../assets/img/brand_section4_img.png";
+
 export const Brand = () => {
   return (
     <section >
@@ -29,8 +32,61 @@ export const Brand = () => {
         </div>
       </div>
 
-      <div className=''>
+      <div className={styles.brand_section3}>
+        <div className='container'>
+          <div className={styles.brand_section3_div}>
+            <div>
+              <img src={brandSection3Img} alt="" />
+            </div>
+            <div>
+              <h2 className={styles.brand_section3_title}>
+                Eigenschaften von ONmacabim cosmetics
+              </h2>
+              <p className={styles.brand_section3_text}>
+                <span> </span> Lösungen für mehrere kosmetische Probleme in einer einzigen Behandlung;
+              </p>
 
+              <p className={styles.brand_section3_text}>
+                <span> </span> Individuelles Herangehen an jeden Kunden, individuelle Behandlungsprotokolle auf der Grundlage der ästhetischen Probleme
+              </p>
+
+              <p className={styles.brand_section3_text}>
+                <span> </span> Kombinierbarkeit mit Gerätetechniken;
+              </p>
+
+              <p className={styles.brand_section3_text}>
+                <span> </span> Vorteil der Verwendung von Präparaten aus verschiedenen Linien in einem Verfahren;
+              </p>
+
+              <p className={styles.brand_section3_text}>
+                <span> </span> Absolute Sicherheit in der Anwendung;
+              </p>
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.brand_section4}>
+        <div className="container">
+          <div className={styles.brand_section4_div}>
+            <div>
+              <h2 className={styles.brand_section3_title}>
+                Herkunft der Pflanzen für ONmacabim Cosmeceuticals
+              </h2>
+              <p>
+                Die Judäische Wüste ist eine einzigartige Region an der Küste des Toten Meeres. Die Kräuter, die in der Region wachsen, sind seit über zweitausend Jahren für ihre heilende Wirkung bekannt.
+              </p>
+              <br />
+              <p>
+                Die rauen klimatischen Bedingungen der Wüste zwingen die Pflanzen dazu, eine große Anzahl nützlicher Inhaltsstoffe anzusammeln und zu bewahren. Alte Manuskripte enthalten häufig Beschreibungen der verschiedenen Möglichkeiten, die Kräuter der judäischen Wüste zur Behandlung verschiedener Krankheiten einzusetzen.
+              </p>
+            </div>
+            <div>
+              <img src={brandSection4Img} alt="" />
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   )
