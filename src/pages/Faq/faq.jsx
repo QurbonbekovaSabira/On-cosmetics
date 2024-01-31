386f530eb0f0d93e01d606fb4e01b2fc0a65ec1b
devdamirbek@gmail.com <developerdamirbek@mail.ru>
2024-01-31 09:38:10 +0500

    new change in FAQS

--- src/pages/Faq/faq.jsx
+++ src/pages/Faq/faq.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import style from './faq.module.scss'
+import { Hero } from '../../components/hero'
 
 export const Faq = () => {
   return (
-    <div>Faq</div>
+    <section className={style.faq}>
+      <Hero 
+      title="Frequently asked questions" 
+      text="Here you can find answers on the most frequently asked questions." />
+    </section>
   )
 }
